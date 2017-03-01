ba496e4b923aed1f1e09553482bcca3dca407e2e
yuliya <yulya.belova_17@mail.ru>
2017-03-01 15:15:46 +0500

    add task about function with sin in studentbook(P 74, ex 5)

---
+++ function_with_sin(p 74, №5)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace function_with_sin
+{
+    class Program
+    {
+        static double Vichislenie(double Chislitel, double Znamenatel)
+        {
+            double result = (Chislitel + Math.Sin(Chislitel)) / (Math.Sin(Znamenatel) + Znamenatel);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            double peremennayaVChislitele = 2, peremennayaVZnamenatele = 5;
+            double resultOfMethod = Vichislenie(peremennayaVChislitele, peremennayaVZnamenatele);
+            double y = 0;
+            y = y + resultOfMethod;
+            peremennayaVChislitele = 6;
+            peremennayaVZnamenatele = 3;
+            resultOfMethod = Vichislenie(peremennayaVChislitele, peremennayaVZnamenatele);
+            y = y + resultOfMethod;
+            peremennayaVChislitele = 1;
+            peremennayaVZnamenatele = 4;
+            resultOfMethod = Vichislenie(peremennayaVChislitele, peremennayaVZnamenatele);
+            y = y + resultOfMethod;
+            Console.WriteLine(y);
+        }
+    }
+}
